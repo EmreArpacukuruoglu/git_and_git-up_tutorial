72bf33ac083322136e039561b69f53592dccdc62
Emre Arpacukuruoglu <arpacukuruogluemre@gmail.com>
2025-03-08 13:49:03 +0300

    saving boilerplate code and text

---
+++ javaScript/ders/app.js
@@ -0,0 +1,8 @@
+const bla=document.querySelector("#emre")
+const buton=document.querySelector("#emree")
+
+buton.addEventListener("click",blau);
+function blau() {
+    bla.style.backgroundColor = "blue";
+  }
+ 
